5b41f9ef00879d42e293f84505681bcb762c40b2
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2025-06-16 01:25:51 -0700

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -64,8 +64,7 @@ export function Intro() {
         <span className="text-red-800 font-light">Insurance</span>
       </h1>
       <div className="mt-4 text-sm/6 text-gray-300">
-        <p>Origin is a recognized leader in Special Investigations Unit (SIU) services for the insurance industry. With a proven track record of uncovering fraudulent claims, securing confessions, and driving claim withdrawals, we provide insurers with the critical intelligence needed to make informed decisions.</p>
-        <p></p>
+        <p className="mb-4">Origin is a recognized leader in Special Investigations Unit (SIU) services for the insurance industry. With a proven track record of uncovering fraudulent claims, securing confessions, and driving claim withdrawals, we provide insurers with the critical intelligence needed to make informed decisions.</p>
         <p>Our team of seasoned investigators combines advanced surveillance techniques, data analysis, and industry expertise to deliver consistent results and protect our clients from financial loss.</p>
       </div>
      {/*  <SignUpForm /> */}
